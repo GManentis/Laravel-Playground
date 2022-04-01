dace245a901a2575c9c75ad5f73f27822174ea31
GManentis <manentis.gerasimos@outlook.com>
2022-04-01 11:24:24 +0300

    check and compare prefixes in comparison to node

--- routes/api.php
+++ routes/api.php
@@ -14,57 +14,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-    /*-------------One to Many---------------- */
-    /*This Works with one to many
+/*-------------One to Many---------------- */
+/*This Works with one to many
     $service = Service::whereHas("products",function($q) {
         $q->where("id",20);
     })->first();
     return $service;
     */
 
-    /*This works ok
+/*This works ok
     $service = Service::find(1);
     $product = $service->products()->find(20); //IT WORKS
     return $product;
     */
 
-    /*This will work ok service 1-->N products  
+/*This will work ok service 1-->N products
     $product = Product::find(20);
     $product->service()->update(["service_en"=>"Test"]);
     return $product->service;
     */
 
-    /*This will work one to many 1 service --> N products
+/*This will work one to many 1 service --> N products
     $service = Service::where("id",1)->first();
     $service->products()->where('id', 20)->update(['price' => 10000]);//returns bool;
     return $service;
     */
 
-    /*1->N
+/*1->N
     $service = Service::where("id",1)->first();
     $service->products()->update(['price' => 10]);//returns bool update will affect all products related to categories;
     return $service;
     */
 
 
-    
-     /*-------------Many to Many---------------- */
-     /*This wont Work in many to many
+
+/*-------------Many to Many---------------- */
+/*This wont Work in many to many
      $appointments = Appointment::wherehas("products",function($q){
         $q->where("id",20);
     })->first();
     return $appointments;
     */
 
-    /*this will work in N --> N    */
-    // $appointments = Appointment::wherehas("products",function($q){
-    //         $q->where("product_id",25);
-    //     })->with('products')->first();
-    //     return $appointments;
+/*this will work in N --> N    */
+// $appointments = Appointment::wherehas("products",function($q){
+//         $q->where("product_id",25);
+//     })->with('products')->first();
+//     return $appointments;
 
-    /*also this will work
+/*also this will work
     $product = Product::find(25);
-    $product->appointments()->find(128);    
+    $product->appointments()->find(128);
     return $product;
     */
 
@@ -72,5 +72,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Or better Route::get("/",[TestController,"ok"]);
-Route::get("/test/{param}","App\Http\Controllers\TestController@ok");
+//Or better Route::get("/test/{param}",[TestController::class,"ok"]);
+Route::get("/test/{param}", "App\Http\Controllers\TestController@ok");
+
+
+
+Route::group(['prefix' => '/testo/{testo}'], function () {
+
+    //in comparison to node both wildcards as passed. in express js the param testo would have been lost and it should be binded to middleware for its maintainance
+    Route::get("/hash/{hash}", function ($testo, $hash) {
+        return $testo . " " . $hash;
+    });
+});
