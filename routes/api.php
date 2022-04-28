055a3ea230c58511367fb806c3fb805ffa09926b
GManentis <manentis.gerasimos@outlook.com>
2022-04-28 20:54:24 +0300

    minor edit note

--- routes/api.php
+++ routes/api.php
@@ -81,8 +81,8 @@ foreach($messages as $message)
 ---------------------
 Second way:
 
-$task = $user->goalobjectives()->where(['goal_objective_id'=>$goal_objective_id,'goal_obj_add_id'=>$goal_obj_add_id])->first(); //get the first record
-$task->pivot->goal_objective_id = $new; //change your col to a new value
+$task = $user->goalobjectives()->where(['goal_objective_id'=>$goal_objective_id])->first(); //get the first record
+$task->pivot->param = $new; //change your col to a new value
 $task->pivot->save(); //save
 
 */
